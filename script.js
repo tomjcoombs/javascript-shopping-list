5c0c03cd6bb6e3b973099c0ac15f12a2639370a9
Tom Coombs <tomjcoombs@gmail.com>
2024-02-03 13:38:39 +0000

    added remove functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
-
+const clearAllButton = document.getElementById('clear');
+const filter = document.getElementById('filter');
+const filterInput = document.getElementById('filter');
 function addItem(e) {
   e.preventDefault();
   const newItem = itemInput.value;
@@ -24,6 +26,7 @@ function addItem(e) {
   itemList.appendChild(li);
 
   itemInput.value = '';
+  checkUI();
 
 }
 
@@ -48,5 +51,66 @@ function createIcon(className) {
     return icon;
 }
 
+function removeItem(e) {
+  console.log(e.target);
+
+  const item = e.target;
+  if(item.tagName === 'I' && item.parentElement.classList.contains("remove-item")) {
+    const button = item.parentElement;
+    const li = button.parentElement;
+
+    if(confirm('are you sure?')) {
+      li.remove();
+      checkUI();
+    }
+  }
+}
+
+function removeAllItems(e) {
+  while(itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  checkUI();  
+}
+
+function checkUI() {
+  const hasListItems = itemList.querySelectorAll('li');
+  // console.log('display', filter.style.display);
+  
+  if(hasListItems.length === 0) {
+    filter.style.display = 'none';
+    clearAllButton.style.display = 'none';
+  } else {
+    filter.style.display = '';
+    clearAllButton.style.display = '';
+  }
+}
+
+let searchTerm = '';
+
+function filterList(e) {
+  // console.log(e.key);
+  searchTerm += e.key;
+
+  // const items = itemList.children;
+  const listItems = itemList.querySelectorAll('li');
+  listItems.forEach(item => {
+
+    const x = item.firstChild;
+    console.log(x.includes(searchTerm));
+    // console.log(item.firstChild.includes(searchTerm));
+    // if (!item.firstChild.includes(searchTerm)) {
+    //   item.style.display = 'none';
+    // }
+  })
+  
+  
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearAllButton.addEventListener('click', removeAllItems);
+window.addEventListener('load', checkUI);
+filterInput.addEventListener('keydown', filterList)
